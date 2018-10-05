94588c97de02058eaaf68d67e046b89659603660
Carlos Segarra <carlossegarragonzalez@gmail.com>
2018-10-05 12:09:22 +0200

    jeje

--- install_packs.sh
+++
@@ -1,6 +0,0 @@
-# This pretends to be an installer for the different packages I usually use in a Debian-Like Distribution
-
-# TODO: check if there is apt-get
-# via wich?
-
-sudo apt-get install spotify-client
