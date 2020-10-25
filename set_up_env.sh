be2f0230c1195bba15bd5956560a6ab0ab5936cf
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-10-25 16:51:44 +0000

    some progress

--- set_up_env.sh
+++ set_up_env.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ### CSG Environment Set Up Script
 # TODO:
 # 1. Add keys to the full installation
@@ -11,58 +13,114 @@
 # 7. If directory already exists, pull
 # 8. Add server mode
 
-# Check if full installation
-if [[ $* == *--full* ]];
-then
-    sudo apt update
-    sudo apt upgrade -y
-    sudo apt install \
-        chromium-browser \
-        xsel
-fi
+## Vars
+DOT_DIR="$HOME/dotfiles/"
+OUT_FILE="${DOT_DIR}/next_steps.txt"
 
+## Helper functions
+function nice_log {
+    echo "--------------------------------------------------------------------------------"
+    echo "\t\t $1"
+    echo "--------------------------------------------------------------------------------"
+}
+
+function log_and_do {
+    echo $1
+    $1
+}
+
+# TODO update via submodule update init
 ## If you want to update and do not have git installed you are dumb
-if [[ $* == *--update* ]];
-then
-    if [ -d "~/dotfiles" ];
-    then
-        cd "~/dotfiles"
-        git pull origin master
-        exit 0
-    else
-        echo "Update but no dotfiles dir!"
-        exit 2
-    fi
-fi
+#if [[ $* == *--update* ]];
+#then
+#    if [ -d "~/dotfiles" ];
+#    then
+#        cd "~/dotfiles"
+#        git pull origin master
+#        exit 0
+#    else
+#        echo "Update but no dotfiles dir!"
+#        exit 2
+#    fi
+#fi
 
 ## General package installation
-sudo apt-get update
-sudo apt-get install -y \
+nice_log "Install general package dependencies..."
+sudo apt update
+sudo apt upgrade
+sudo apt install -y \
+    apt-transport-https \
     ctags \
     curl \
+    dmenu \
     git \
+    gnupg2 \
+    gnupg-agent \
     python-dev \
     pyton-pip \
     python3-dev \
     pyton3-pip \
+    software-properties-common \
     ssh \
     sudo \
     tmux \
+    wget \
+    xsel \
     zsh
+
+## Clone the dotfiles repo
+nice_log "Cloning dotfiles repo..."
+git clone git@github.com:csegarragonz/dotfiles.git ${DOT_DIR}
+git submodule update --init
+cd ${DOT_DIR}
+
+## External dependencies (not tracked)
+# dmenu - suckless tools
+nice_log "Installing suckless tools..."
+sudo apt install -y \
+    libx11-dev \
+    libxft-dev \
+    libxinerama1 \
+    libxinerama-dev
+pushd ${DOT_DIR}/third-party >> /dev/null
+wget https://dl.suckless.org/tools/dmenu-5.0.tar.gz
+tar -xzvf dmenu-5.0.tar.gz
+rm dmenu-5.0.tar.gz; cd dmenu-5.0
+sudo make clean install
+popd >> /dev/null
+# st - suckless terminal
+pushd  ${DOT_DIR}/third-party/st/ >> /dev/null
+sudo make install
+popd >> /dev/null
 
 ## Update default shell
 chsh -s $(which zsh)
-# TODO link zsh things
+ln -s ${DOT_DIR}/zsh/.zshrc ~/.zshrc
+source ~/.zshrc
 # TODO need to reboot!
 
-## Link .profile file
-ln -s ~/dotfiles/.profile ~/.profile
-
-## Global Gitignore
-git config --global core.excludesfile ~/dotfiles/git/.gitignore_global
+## Git config
+# Create ssh public key
+nice_log "Create ssh public key"
+ssh-keygen -t rsa
+# Global Gitignore
+nice_log "Git configuration"
+git config --global user.name "Carlos Segarra"
+git config --global user.email "carlos@carlossegarra.com"
+git config --global core.excludesfile ${DOT_DIR}/git/.gitignore_global
+echo "Install the new public key in Github"
+cat ~/.ssh/id_rsa.pub >> ${OUT_FILE}
 
 ## Install Docker
-# TODO installation steps
+nice_log "Install docker"
+sudo apt remove docker docker-engine docker.io containerd runc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
 # Post-installation
 sudo groupadd docker
 sudo usermod -aG docker $USER
@@ -70,22 +128,11 @@ newgrp docker
 
 ## Neovim configuration
 # Install Neovim
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
-# Installl Neovim with python support
-sudo apt-get install
-pip3 install user neovim
-pip3 install user neovim
+nice_log "Installing neovim...."
+sudo apt-get install neovim python3 neovim
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Clone the repo with the config files
-cd ~
-git clone git@github.com:csegarragonz/dotfiles.git
-
 # Setting up nvim
 mkdir -p .config/nvim/
 ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
@@ -94,29 +141,14 @@ ln -s ~/dotfiles/nvim/syntax ~/.config/nvim/
 nvim +PlugInstall +qa
 nvim +PlugUpdate +qa
 
-# Tmux configuration
- Setting up tmux
-if [ -f '~/.tmux.conf'];
-then
-    echo "There already exists a tmux configuration file. Replace it?"
-    # TODO: ask Y/N
-else
-    ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-    ln -s ~/dotfiles/.tmux ~/.tmux
-    # Install TPM and the plugins contained in the conf file
-    git clone https://github.com/tmux-plugins/tpm ~/dotfiles/.tmux/plugins/tpm
-    tmux source ~/.tmux.conf
-    ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-fi
-
-## Set up Suckless Terminal
-sudo apt install libxft-dev libx11-dev
-# Change dir TODO
-git clone git@github.com:csegarragonz/st.git
-cd st
-sudo make install
+## Tmux configuration
+nice_log "Setting up tmux..."
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.tmux ~/.tmux
 
 ## Gnome changes
+nice_log "Setting up gnome specific things..."
+sudo apt install gnome-tweaks
 # Disable the annoying notifications on the top bar
 gnome-extensions disable ubuntu-appindicators@ubuntu.com
 # Force alt+tab to switch only in current workspace
